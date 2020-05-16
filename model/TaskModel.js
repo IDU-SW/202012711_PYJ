edf22b6cc8b555625bbeea056bc4ee8bf58ae6b9
p41155a <50395024+p41155a@users.noreply.github.com>
2020-05-17 05:00:23 +0900

    sequelize/model

--- model/TaskModel.js
+++ model/TaskModel.js
@@ -1,100 +1,134 @@
 const fs = require('fs');
-const pool = require('../dbConnection');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('example', 'dev', '1234', {dialect:'mysql', host:'127.0.0.1'});
+
+class Tasks extends Sequelize.Model {}
+Tasks.init({
+    id: {
+        type: Sequelize.INTEGER,
+        unique: true,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    task: Sequelize.STRING,
+    subject: Sequelize.STRING,
+    deadline: Sequelize.STRING,
+    done: Sequelize.STRING,
+}, {tableName:'tasks', sequelize, timestamps: false});
 
 class Task {
-    /*
     constructor() {
+        try {
+            this.prepareModel(); 
+        } catch (error) {
+            console.error(error);    
+        }
+    }
+
+    async prepareModel() {
+        try {
+            await Tasks.sync({force:true});
+            await this.preDataInsert();
+        }
+        catch (error) {
+            console.log('prepareModel Error', error);
+        }
+    }
+
+    async preDataInsert() {
         const data = fs.readFileSync('./model/data.json');
-        this.tasks = JSON.parse(data)
-    }
-    */
-    getTaskList = async() => {
-        let conn;
-        const sql = 'SELECT * FROM tasks';
-        
-        try {
-            conn = await pool.getConnection();
-            const [rows, metadata] = await conn.query(sql);
-            conn.release();
-            return rows;
-        } catch (error) {
-            console.error(error);
-        } finally {
-            if ( conn ) conn.release();
+        const tasks = JSON.parse(data);
+        for (var task of tasks ) {
+            await this.oneDataInsert(task);
         }
     }
 
-    getTaskDetail = async(taskId) => {
-        let conn;
-        const sql = 'SELECT * FROM tasks WHERE id = ?';
-
+    async oneDataInsert(newTask) {
         try {
-            conn = await pool.getConnection();
-            const [rows, metadata] = await conn.query(sql, taskId);
-            for(let row of rows) {
-                conn.release();
-                return row;
-            }        
-        } catch (error) {
-            console.error(error);
-        } finally {
-            if ( conn )
-                conn.release();
-        }
-    }
-
-    addTask = async(add_task, add_subject, add_deadline, add_done) => {
-        let conn;
-        const sql1 = 'INSERT INTO tasks SET ?;';
-        const sql2 = 'SELECT * FROM tasks WHERE id = ?';
-
-        try {
-            conn = await pool.getConnection();
-            const data = { task: add_task, subject: add_subject, deadline: add_deadline, done: add_done};
-            const ret = await conn.query(sql1, data);
-            const [rows, metadata] = await conn.query(sql2, ret[0]['insertId']);
-            for(let row of rows) {
-                conn.release();
-                return row;
-            }         
-        } catch (error) {
-            console.error(error);
-        } finally {
-            if ( conn )
-                conn.release();
-        }
-    }
-
-    updateTask = async(taskId, task, subject, deadline, done) => {
-        let conn;
-        const sql = 'UPDATE tasks SET ? WHERE id = ?';
-        const param = {task: task, subject: subject, deadline: deadline, done: done};
-        const condition = taskId;
-        
-        try {
-            conn = await pool.getConnection();
-            const ret = await conn.query(sql, [param, condition] );
-            conn.release();
+            const ret = await Tasks.create({
+                task : newTask.task,
+                subject : newTask.subject,
+                deadline : newTask.deadline,
+                done : newTask.done
+            }, {logging: false});
             return ret;
-        } catch (error) {
-            console.error(error);  
-        } finally {
-            if ( conn ) conn.release();
+        }
+        catch (error) {
+            console.log('Error : ', error);
         }
     }
 
-    deleteTask = async(taskId) => {
-        let conn;
-        const sql = 'DELETE FROM tasks WHERE id = ?';
-        
+    async getTaskList() {
+        let ret;
+        await Tasks.findAll({})
+        .then( results => {
+            for (var item of results) {
+                console.log('id:', item.id, ' task:', item.task);
+            }
+            ret = results;
+        })
+        .catch( error => {
+            console.error('Error :', error);
+        });
+        return ret;
+    }
+
+    async getTaskDetail(taskId) {
         try {
-            conn = await pool.getConnection();        
-            const ret = await conn.query(sql, taskId);
+            // Primary Key로 찾기
+            let results = await Tasks.findAll({ where:{id:taskId}});
+            if ( results ) {
+                return results[0];
+            }
+            else {
+                console.log('no data');
+            }
+        }
+        catch (error) {
+            console.log('Error :', error);
+        }
+    }
+
+    async addTask(add_task, add_subject, add_deadline, add_done) {
+        try {
+            const ret = await Tasks.create({
+                task : add_task,
+                subject : add_subject,
+                deadline : add_deadline,
+                done : add_done
+            }, {logging: false});
             return ret;
-        } catch (error) {
-            console.error(error);  
-        } finally {
-            if ( conn ) conn.release();
+        }
+        catch (error) {
+            console.log('Error : ', error);
+        }
+    }
+
+    async updateTask(taskId, task, subject, deadline, done) {
+        try {
+            let updateTask = await Tasks.findByPk(taskId);
+            updateTask.task = task
+            updateTask.subject = subject
+            updateTask.deadline = deadline
+            updateTask.done = done
+            
+            let ret = await updateTask.save();
+            let changedTask = ret.dataValues;
+            console.log('ret :',changedTask);
+        }
+        catch (error) {
+            console.log('Error :', error);
+        }
+    }
+
+    async deleteTask(taskId) {
+        try {
+            let result = await Tasks.destroy({ where: {id:taskId}});
+            console.log('Remove success :', result);
+        }
+        catch (error) {
+            console.log('Remove Error :', error);
         }
     }
 }
